87257c73990a1ac26e5674739d0f9f2b7d3c9023
= <=>
2025-06-28 12:43:54 -0700

    adding hero

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 import "./index.css";
 import gsap from "gsap";
 import Nav from "./Nav";
+import Hero from "./Hero";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 function App() {
   return (
     <>
       <main>
         <Nav />
+        <Hero />
+        <div className="h-dvh bg-black"></div>
       </main>
     </>
   );
