d2aaac9b61838b6d3a71b2b129292c7d26421820
= <=>
2025-06-28 11:33:26 -0700

    NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { ScrollTrigger, SplitText } from "gsap/all";
 import "./index.css";
 import gsap from "gsap";
+import Nav from "./Nav";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 function App() {
   return (
     <>
-      <div className="flex-center h-[100vh]">
-        <h1 className="text-7xl text-indigo-300">Hello GSAP!</h1>
-      </div>
+      <main>
+        <Nav />
+      </main>
     </>
   );
 }
