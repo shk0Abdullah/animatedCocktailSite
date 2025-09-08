1847a660b2432700975d84793e8bb1202e46e37c
Abdullah Amjad <shkabdullah268@gmail.com>
2025-09-08 08:44:44 -0700

    ..

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { openingHours, socials } from "../../constants";
 import { useGSAP } from "@gsap/react";
-import SplitText from "gsap/all";
+import { SplitText } from "gsap/all";
 import gsap from "gsap";
 const Contact = () => {
   useGSAP(() => {
